6751430061cba00115ad0953963f35ce1bcf924b
Xstinkshun <61350672+siddhanth78@users.noreply.github.com>
2020-08-18 21:22:59 +0530

    new interface

--- OTPStudentAttendance/Attendance/Student.py
+++ OTPStudentAttendance/Attendance/Student.py
@@ -12,38 +12,110 @@ class win:
 
         self.cursor = self.db.cursor()
         
-        self.master.geometry("500x120+500+300")
+        self.master.geometry("500x200+500+300")
         self.master.title("Attendance")
+        self.master.configure(background='black')
         
         win.v1 = StringVar()
         win.v2 = StringVar()
         win.v3 = StringVar()
 
-        self.titlelab = Label(self.master,text = "Student Attendance",width=25)
-        self.titlelab.grid(row=0,column=0)
+        self.titlelab = Label(self.master,text = "Press 'Enter' to lock your response",width=30,justify=CENTER,bg='black',fg='orange')
+        self.titlelab.pack(anchor=CENTER)
 
-        self.idlab = Label(self.master,text = "Teacher ID",width=25)
-        self.idlab.grid(row=1,column=0)
+        self.seplab1 = Label(master,bg='black')
+        self.seplab1.pack(anchor=CENTER)
 
-        self.identry = Entry(self.master,textvariable=win.v3,width=20,relief=SUNKEN)
-        self.identry.grid(row=1,column=1)
+        self.tidentry = Entry(self.master,textvariable=win.v3,width=30,relief=SUNKEN,justify=CENTER,bg='black',fg='orange')
+        self.tidentry.insert(0,'Teacher ID')
+        self.tidentry.bind("<Enter>",self.tidenter)
+        self.tidentry.bind("<Leave>",self.tidleave)
+        self.tidentry.bind("<Return>",self.tidclose)
+        self.tidentry.pack(anchor=CENTER)
 
-        self.idlab = Label(self.master,text = "Student ID",width=25)
-        self.idlab.grid(row=2,column=0)
+        self.identry = Entry(self.master,textvariable=win.v1,width=30,relief=SUNKEN,justify=CENTER,bg='black',fg='orange')
+        self.identry.insert(0,'Student ID')
+        self.identry.bind("<Enter>",self.identer)
+        self.identry.bind("<Leave>",self.idleave)
+        self.identry.bind("<Return>",self.idclose)
+        self.identry.pack(anchor=CENTER)
 
-        self.identry = Entry(self.master,textvariable=win.v1,width=20,relief=SUNKEN)
-        self.identry.grid(row=2,column=1)
+        self.otpentry = Entry(self.master,textvariable=win.v2,width=30,relief=SUNKEN,justify=CENTER,bg='black',fg='orange')
+        self.otpentry.insert(0,'OTP')
+        self.otpentry.bind("<Enter>",self.otpenter)
+        self.otpentry.bind("<Leave>",self.otpleave)
+        self.otpentry.bind("<Return>",self.otpclose)
+        self.otpentry.pack(anchor=CENTER)
 
-        self.otplab = Label(self.master,text = "OTP",width=25)
-        self.otplab.grid(row=3,column=0)
+        self.seplab2 = Label(master,bg='black')
+        self.seplab2.pack(anchor=CENTER)
 
-        self.otpentry = Entry(self.master,textvariable=win.v2,width=20,relief=SUNKEN)
-        self.otpentry.grid(row=3,column=1)
-
-        self.enterbutton = Button(self.master,text = "OK",width=25,command=self.verification)
-        self.enterbutton.grid(row=4,column=0)
+        self.enterbutton = Button(self.master,text = "OK",width=30,command=self.verification,bg='black',fg='orange')
+        self.enterbutton.bind("<Enter>",self.butenter)
+        self.enterbutton.bind("<Leave>",self.butleave)
+        self.enterbutton.pack(anchor=CENTER)
 
         self.msgbox = messagebox
+
+
+    def identer(self,event):
+        self.identry.config(fg='black',bg='orange')
+        self.identry.delete(0,'end')
+
+    def idleave(self,event):
+        self.identry.delete(0,'end')
+        self.identry.insert(0,'Student ID')
+        self.identry.config(bg='black',fg='orange')
+
+    def idclose(self,event):
+        ident = self.identry.get()
+        if ident.strip()=='':
+            return
+        self.identry.config(bg='black',fg='orange')
+        self.identry.unbind("<Enter>")
+        self.identry.unbind("<Leave>")
+
+
+    def tidenter(self,event):
+        self.tidentry.config(fg='black',bg='orange')
+        self.tidentry.delete(0,'end')
+
+    def tidleave(self,event):
+        self.tidentry.delete(0,'end')
+        self.tidentry.insert(0,'Teacher ID')
+        self.tidentry.config(bg='black',fg='orange')
+
+    def tidclose(self,event):
+        tident = self.tidentry.get()
+        if tident.strip()=='':
+            return
+        self.tidentry.config(bg='black',fg='orange')
+        self.tidentry.unbind("<Enter>")
+        self.tidentry.unbind("<Leave>")
+
+    def otpenter(self,event):
+        self.otpentry.config(fg='black',bg='orange')
+        self.otpentry.delete(0,'end')
+
+    def otpleave(self,event):
+        self.otpentry.delete(0,'end')
+        self.otpentry.insert(0,'OTP')
+        self.otpentry.config(bg='black',fg='orange')
+
+    def otpclose(self,event):
+        otpent = self.otpentry.get()
+        if otpent.strip()=='':
+            return
+        self.otpentry.config(bg='black',fg='orange')
+        self.otpentry.unbind("<Enter>")
+        self.otpentry.unbind("<Leave>")
+
+
+    def butenter(self,event):
+        self.enterbutton.config(fg='black',bg='orange')
+
+    def butleave(self,event):
+        self.enterbutton.config(fg='orange',bg='black')
     
 
     def verification(self):
@@ -51,11 +123,29 @@ class win:
         listud=[]
         liteach=[]
         sid = win.v1.get()
-        sid = sid.strip()
+        sid = sid.strip().upper()
         sotp = win.v2.get()
         sotp = sotp.strip()
         tid = win.v3.get()
-        tid = tid.strip()
+        tid = tid.strip().upper()
+
+        if tid == 'Teacher ID'.upper():
+            self.msgbox.showwarning("Invalid","Check the information submitted and try again.")
+            self.tidentry.bind("<Enter>",self.tidenter)
+            self.tidentry.bind("<Leave>",self.tidleave)
+            return
+
+        if sid == 'Student ID'.upper():
+            self.msgbox.showwarning("Invalid","Check the information submitted and try again.")
+            self.identry.bind("<Enter>",self.identer)
+            self.identry.bind("<Leave>",self.idleave)
+            return
+
+        if sotp == 'OTP':
+            self.msgbox.showwarning("Invalid","Check the information submitted and try again.")
+            self.otpentry.bind("<Enter>",self.otpenter)
+            self.otpentry.bind("<Leave>",self.otpleave)
+            return
         
         self.cursor.execute("select id from studentlist")
         
@@ -63,6 +153,8 @@ class win:
             listud.append(x[0])
         if sid not in listud:
             self.msgbox.showwarning("Invalid","Check credentials and try again.")
+            self.identry.bind("<Enter>",self.identer)
+            self.identry.bind("<Leave>",self.idleave)
             return
         else:
             pass
@@ -73,6 +165,8 @@ class win:
             liteach.append(x[0])
         if tid not in liteach:
             self.msgbox.showwarning("Invalid","Check credentials and try again.")
+            self.tidentry.bind("<Enter>",self.tidenter)
+            self.tidentry.bind("<Leave>",self.tidleave)
             return
         else:
             pass
@@ -124,6 +218,8 @@ class win:
             self.master.destroy()
         else:
             self.msgbox.showwarning("Invalid","Check credentials and try again.")
+            self.otpentry.bind("<Enter>",self.otpenter)
+            self.otpentry.bind("<Leave>",self.otpleave)
             return
         
 
